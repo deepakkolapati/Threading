afb9400908e54ec8a584064184eef83fd434ff15
deepakkolapati <deepakchandu9@gmail.com>
2025-08-15 18:55:17 +0530

    moved main function logic

--- src/com/deepak/main/ProducerConsumer.java
+++ src/com/deepak/main/ProducerConsumer.java
@@ -30,4 +30,32 @@ public class ProducerConsumer {
         this.capacity = capacity;
 
     }
+
+    public static void main(String[] args) {
+        ProducerConsumer pc = new ProducerConsumer(10);
+        Thread producer = new Thread(()-> {
+            try{
+                for(int i =1; i<=10;i++){
+                    pc.produce(i);
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+        Thread consumer = new Thread(()->
+        {try{
+            for(int i =1;i<=10;i++){
+                pc.consume();
+                Thread.sleep(150);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        });
+
+        producer.start();
+        consumer.start();
+    }
 }
